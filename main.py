16b997cf3afd8f3b01aac93ebde1eab2e3a45ba2
16232 <1623255609@qq.com>
2024-12-24 10:34:24 +0800

    Changes

--- main.py
+++ main.py
@@ -1,62 +1,95 @@
-# 爬取B站的视频 Bilibili
-# B站的视频地址是直接存储在源网页中的，因此只需要从源网页中解析即可
-
 import requests
-import json
 import re
-from pprint import pprint
-
 import os
+import json
+import subprocess
+from bs4 import BeautifulSoup
 
-# 选择一个视频 ，只需要前半部分就可以啦
-url=r'https://www.bilibili.com/video/BV1ZH4y1R7fS/'
 
-# 设置请求头
-header={
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76",
-    "Referer":"https://www.bilibili.com/", #设置防盗链
+if not os.path.exists('./b_video'):
+    os.mkdir('./b_video')
+headers = {
+    # 获取最高清1080p数据,加入cookie个人信息
+    'Cookie': 'buvid3=E0CC3B70-C034-5F99-108B-417EAAB6D58D72208infoc; b_nut=1713876972; _uuid=787A410510-29D6-3E12-CA3F-B34193EB71D473899infoc; buvid4=0E1C561F-6DF8-B44C-DF45-45C180B3AEF473881-024042312-A0qBauS20OKwBtcJZSUxzWEO4CNtd2qdxh3XvdPkMx%2FKI6%2BT71OvmVbVmN3eRp3P; enable_web_push=DISABLE; FEED_LIVE_VERSION=V_WATCHLATER_PIP_WINDOW3; rpdid=0zbfAHUo63|11buTGIeY|2D|3w1RZfHa; header_theme_version=CLOSE; DedeUserID=442663406; DedeUserID__ckMd5=30156e8f6d52e5c9; buvid_fp_plain=undefined; LIVE_BUVID=AUTO4017241252399556; SESSDATA=3cbf9059%2C1744699099%2Cf005d%2Aa1CjDXidLlhcVQXR2HJMsuiJC3VkrBakxgwqwJxoUEyxO4niemqZCfdbUOhexvScrim8MSVkdNM05WSng4Vmtfd1M2c19SalE3a25zYkp5eU1xOEhKZUU4a2w5Q3FLUlJUSU5ya1Q4VHE5Y1p6bmVzMElNazFndnhILWRZSW1leGFSaE12THZXbVdnIIEC; bili_jct=4c281368a7f9090515b851a60880ccc1; home_feed_column=5; browser_resolution=1545-825; blackside_state=0; CURRENT_BLACKGAP=0; fingerprint=adcfd40cacc10584fc9742a0bf252c85; buvid_fp=adcfd40cacc10584fc9742a0bf252c85; CURRENT_QUALITY=80; CURRENT_FNVAL=4048; bp_t_offset_442663406=1012124732817932288; b_lsid=F106454FB_193F62BDD10; sid=5914pnm3; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzUyNjMyOTAsImlhdCI6MTczNTAwNDAzMCwicGx0IjotMX0.le6Ukbm15K5jiL1tPu2zn8g0SFALGNzzDXoL-2cqcho; bili_ticket_expires=1735263230; bsource=search_google; bmg_af_switch=1; bmg_src_def_domain=i2.hdslb.com'
+    ,
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76"
+    ,
+    'referer': 'https://www.bilibili.com/?spm_id_from=333.1365.0.0'
 }
 
-resp=requests.get(url=url,headers=header)
-# print(resp.text) #成功获取
 
-# 这个baseUrl就是视频的地址
+def get_resopnse(html_url):
+    response = requests.get(url=html_url, headers=headers)
+    return response
 
-obj=re.compile(r'window.__playinfo__=(.*?)</script>',re.S)
-html_data=obj.findall(resp.text)[0] #从列表转换为字符串
-# print(html_data)
-# 转化为字典的形式
-json_data=json.loads(html_data)
-# 格式化输出
-# pprint(json_data)
-# video 和 audio分别是视频和音频 因此爬取下来以后，还需要将两个合并
 
-videos=json_data['data']['dash']['video']  #这里得到的是一个列表
-# 只需要baseUrl即可
-# print(videos)
-video_url=videos[0]['baseUrl'] #视频地址
+# 获取视频标题
+def get_viedeo_info(html_url):
+    resopnse = get_resopnse(html_url)
+    soup = BeautifulSoup(resopnse.text, 'html.parser')
+    title_tag = soup.find('h1', class_='video-title')
 
-# 同理，音频地址为
+    if title_tag:
+        title = title_tag.get('title', '').replace(' ', '')
+        video_info = [title]
+        return video_info
+    else:
+        print("未能找到视频标题")
+        return None
 
-audios=json_data['data']['dash']['audio']
-audio_url=audios[0]['baseUrl']
 
-resp1=requests.get(url=video_url,headers=header)
+# 获取视频的视频和音频的链接
+def get_video_content(BV_ID, spm_id_from, vd_source):
+    index_url = 'https://www.bilibili.com/video/' + BV_ID + '/'
+    data = {
+        'spm_id_from': spm_id_from,
+        'vd_source': vd_source
+    }
+    page_text = requests.get(url=index_url, params=data, headers=headers).text
+    # window.__playinfo__ =({.*?})
+    # (.*?只能匹配字符不能匹配符号,例如'{}')
+    ex = r'window\.__playinfo__=({.*?})\s*</script>'
+    json_data = re.findall(ex, page_text)[0]
+    # json字符串转换为python对象
+    data = json.loads(json_data)
+    audio_url = data['data']['dash']['audio'][0]['baseUrl']
+    video_url = data['data']['dash']['video'][0]['baseUrl']
+    video_content = [audio_url, video_url]
+    return video_content
 
-with open('test.mp4',mode='wb') as f:
-    f.write(resp1.content)
 
-resp2=requests.get(url=audio_url,headers=header)
+# 数据保存
+def save(title, audio_url, video_url):
+    audio_content = get_resopnse(audio_url).content
+    video_content = get_resopnse(video_url).content
+    fp = open(title + '.mp3', mode='wb')
+    fp.write(audio_content)
+    fp = open(title + '.mp4', mode='ab')
+    fp.write(video_content)
+    print(title, '保存完成')
 
-with open('test.mp3',mode='wb') as f:
-    f.write(resp2.content)
 
-# print("爬取完成")
-# 现在需要将视频和音频合并 需要模块ffmpeg 可以在网上看教程
+# 音视频合成
+def merge_data(video_name):
+    '''数据合并'''
+    cmd = rf"ffmpeg -i {video_name}.mp4 -i {video_name}.mp3 -acodec copy -vcodec copy ./out/{video_name}out.mp4"
+    subprocess.run(cmd, shell=True)
 
-command=r'ffmpeg -i test.mp4 -i test.mp3 -acodec copy -vcodec copy testout.mp4'
 
-exit_code = os.system(command)
-if exit_code != 0:
-    print(f"ffmpeg command failed with exit code {exit_code}")
-# 做到这一步即成功了！！！！
+def main(BV_ID, spm_id_from, vd_source):
+    html_url = f'https://www.bilibili.com/video/{BV_ID}/?spm_id_from={spm_id_from}&vd_source={vd_source}'
+    video_info = get_viedeo_info(html_url)
+    video_content = get_video_content(BV_ID, spm_id_from, vd_source)
+    save(video_info[0], video_content[0], video_content[1])
+    merge_data(video_info[0])
+
+
+b_url = input('输入爬取视频的网址:')
+# b_url='https://www.bilibili.com/video/BV1et421a7E1/?spm_id_from=333.337.search-card.all.click&vd_source=77c296188837a388ccd6343ff122de09'
+# 取出参数,BV_ID,spm,_id_from,vd_source
+list = b_url.split('/')
+BV_ID = list[4]
+new_list = list[5].split('=')
+spm_id_from = new_list[1].split('&')[0]
+vd_source = new_list[-1]
+main(BV_ID, spm_id_from, vd_source)
